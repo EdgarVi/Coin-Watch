ece039b1d7b9ed75aac908be2992bc110dc15065
evillase <evillase@eecs.wsu.edu>
2018-10-26 15:48:02 -0700

    Created project skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+// App is a functional component, no need for state
+const App = () => {
+    return(
+        <div className="App">
+            <h1>Coin Watch js Halloween themed yuuuhhhh</h1> 
+            <div className="body"/>
+        </div>
+    );
+    }
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <div className="App">
+            <h1>Coin Watch js Halloween themed yuuuhhhh</h1> 
+            <div className="body"/>
+        </div>
+    );
+    }
+
+export default App;
