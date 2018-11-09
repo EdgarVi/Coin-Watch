e9417391c7150fb2738dac2b0cdb60b79e918f93
evillase <evillase@eecs.wsu.edu>
2018-11-09 14:00:41 -0800

    Added styling to coin display

--- src/components/CoinDisplay/CoinDisplay.js
+++ src/components/CoinDisplay/CoinDisplay.js
@@ -70,8 +70,37 @@ class CoinDisplay extends Component {
             return<h1>{error}</h1>
         }
         return (
-            <div className="coinDisplay">
-                <h1>{this.state.coin.name}</h1>
+            <div>
+                <div className="coinDisplay">
+                    <h1 className="Display-heading"><img src = {this.state.coin.iconUrl} 
+                    alt = "" width = "23" height = "23"></img> {this.state.coin.name} ({this.state.coin.symbol})</h1>
+                    <div className="Display-container">
+                        <div className="Display-item">
+                            Price <span className="Display-value">{this.state.base.sign} {this.state.coin.price}</span>
+                        </div>
+                        <div className="Display-item">
+                            Rank <span className="Display-value">{this.state.coin.rank}</span>
+                        </div>
+                        <div className="Display-item">
+                            24H Change
+                            <span className="Display-value">{this.state.coin.change}</span>
+                        </div>
+                        <div className="Display-item">
+                            <span className="Display-title">Market cap</span>
+                            <span className="Display-dollar">$</span>
+                            {this.state.coin.marketCap}
+                        </div>
+                        <div className="Display-item">
+                            <span className="Display-title">24H Volume</span>
+                            <span className="Display-dollar">{this.state.base.sign}</span>
+                            {this.state.coin.volume}
+                        </div>
+                        <div className="Display-item">
+                            <span className="Display-title">Total supply</span>
+                            {this.state.coin.totalSupply} {this.state.coin.symbol}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
