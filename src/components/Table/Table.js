92b3fef09983e051427d3cd85577f63012537a5d
evillase <edgar.villasenor@wsu.edu>
2019-05-21 10:03:10 -0800

    Nav button styling

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -24,7 +24,7 @@ const Table = (props) => {
                         onClick={() => props.history.push(`/coin/${coin.id}`)}
                     >
                         <th>{coin.rank}</th>  
-                        <th className="coin"><img src = {coin.iconUrl} alt = "" width = "23" height = "23"></img> {coin.name}</th>
+                        <th className="coin"><img src = {coin.iconUrl} alt = "" width = "23" height = "23"></img>&nbsp;&nbsp;&nbsp;&nbsp;{coin.name}</th>
                         <th>{props.base.sign}{Number(coin.price).toFixed(2)}</th>
                         <th>{props.base.sign}{(coin.marketCap).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</th>
                         {(coin.change > 0) ? 
