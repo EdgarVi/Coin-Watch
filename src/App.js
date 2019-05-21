215f9cedb3a802b49f267160a6df7575db12641b
evillase <edgar.villasenor@wsu.edu>
2019-05-20 21:52:22 -0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import List from './components/Table/List';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import NotFound from './components/NotFound';
@@ -16,6 +17,7 @@ const App = () => {
                     <Route path = "/coin/:id" component = {CoinDisplay} exact/>
                     <Route component = {NotFound}/>
                 </Switch>
+                <Footer/>
             </div>
         </BrowserRouter>
     );  
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <div className="Footer"> 
+            <div className="Data">
+                <p id="e1">Data provided by &nbsp;</p>
+                <a id="e2" href="https://docs.coinranking.com/">Coinranking</a>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Footer;
